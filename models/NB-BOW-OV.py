0f2fb20b84319b32a8b2f5e39cf8c449dcb4f7bb
KZ <ng_khang_zheng@hotmail.com>
2020-12-05 05:42:30 -0500

    Update NB-BOW-OV.py

--- models/NB-BOW-OV.py
+++ models/NB-BOW-OV.py
@@ -17,5 +17,5 @@ for row in train_list:
 word_collections_counter = collections.Counter()
 for row in train_set_2D_list:
     word_collections_counter.update(row[1].split(" "))
-# a filtered two-dimensional list (list of tuple objects with word and word frequencies)
+# an original two-dimensional list (list of tuple objects with word and word frequencies)
 original_vocabulary_list = list(word_collections_counter.items())
