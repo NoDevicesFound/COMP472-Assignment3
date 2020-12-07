b53b838ed586c029f1f892fa8abe6b080cc8af3e
KZ <ng_khang_zheng@hotmail.com>
2020-12-06 20:15:18 -0500

    reworked using CountVectorizer(), TfidfTransformer(), and MultinomialNB(). Add TODO

--- models/NB-BOW-FV.py
+++ models/NB-BOW-FV.py
@@ -1,9 +1,17 @@
-import collections
+import numpy as np
+from sklearn import metrics
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+
+# ref: https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
 
 # a 2d list of training sets, where each index should contains array of 3 elements
-train_set_2D_list = []
-
-# read file with utf-8
+train_tweet_id_list = []
+train_text_list = []
+train_q1_label_list = []
+# read train file with utf-8
 train_file = open("data/covid_training.tsv", "r", encoding="utf-8")
 train_list = train_file.readlines()
 train_file.close()
@@ -12,14 +20,53 @@ del train_list[0]
 # put every training set with tweet_id, text, and q1_label into 2d array
 for row in train_list:
     train_string_list = str(row).split("\t")
-    train_set_2D_list.append([train_string_list[0], train_string_list[1].lower(), train_string_list[2]])
-# class collections.Counter to count word in all tweet texts
-word_collections_counter = collections.Counter()
-for row in train_set_2D_list:
-    word_collections_counter.update(row[1].split(" "))
-# Filter out words that appear only once in the training set
-for key, value in list(word_collections_counter.items()):
-    if value == 1:
-        del word_collections_counter[key]
-# a filtered two-dimensional list (list of tuple objects with word and word frequencies)
-filtered_vocabulary_list = list(word_collections_counter.items())
+    train_tweet_id_list.append(train_string_list[0])
+    train_text_list.append(train_string_list[1].lower())
+    train_q1_label_list.append(train_string_list[2])
+# Tokenizing text with scikit-learn, min_df ignore words that have less than 2 occurrences
+count_vect = CountVectorizer(min_df=2)
+X_train_counts = count_vect.fit_transform(train_text_list)
+# From occurrences to frequencies
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+# read test file with utf-8
+test_file = open("data/covid_test_public.tsv", "r", encoding="utf-8")
+test_list = test_file.readlines()
+test_file.close()
+# a 2d list of testing sets, where each index should contains array of 3 elements
+test_tweet_id_list = []
+test_text_list = []
+test_q1_label_list = []
+# put every testing set with tweet_id, text, and q1_label into 2d array
+for row in test_list:
+    test_string_list = str(row).split("\t")
+    test_tweet_id_list.append(test_string_list[0])
+    test_text_list.append(test_string_list[1].lower())
+    test_q1_label_list.append(test_string_list[2])
+
+# Training a classifier
+clf = MultinomialNB(alpha=0.01).fit(X_train_tfidf, train_q1_label_list)
+
+X_new_counts = count_vect.transform(test_text_list)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+predicted = clf.predict(X_new_tfidf)
+
+# Output
+output_trace_file = open("output/trace_NB-BOW-FV.txt", "w")
+# TODO 2.2.2 3. per-class recall (yes-R, no-R)
+for id, predict, result in zip(test_tweet_id_list, predicted, test_q1_label_list):
+    output_trace_file.writelines(id + "  " + predict + "  " + score? + "  " + ("correct" if (predict == result) else "wrong") + "\r")
+output_trace_file.close()
+
+# https://scikit-learn.org/stable/modules/classes.html#sklearn-metrics-metrics
+# TODO fix precision_score, recall_score, f1_score
+output_eval_file = open("output/eval_NB-BOW-FV.txt", "w")
+output_eval_file.write(
+    metrics.accuracy_score(test_q1_label_list, predicted.tolist()) + "\r" +
+    metrics.precision_score(test_q1_label_list, predicted.tolist()) + "\r" +
+    metrics.recall_score(test_q1_label_list, predicted.tolist()) + "\r" +
+    metrics.f1_score(test_q1_label_list, predicted.tolist()) + "\r"
+)
+output_eval_file.close()
