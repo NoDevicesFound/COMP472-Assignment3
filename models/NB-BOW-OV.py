4e03069f10594ab6aafa30e578b977c0fd7ca8e4
KZ <ng_khang_zheng@hotmail.com>
2020-12-13 22:40:31 -0500

    change log base to 10

--- models/NB-BOW-OV.py
+++ models/NB-BOW-OV.py
@@ -1,11 +1,10 @@
-# %%
 import math
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score, classification_report, confusion_matrix
 
-# %%
 def word_tokenization(text):
     vectorizer = CountVectorizer()
     
@@ -22,7 +21,6 @@ def word_tokenization(text):
             features_text[r][c] = (tokenized_names[c], tokenized_text[r][c])
     return features_text
 
-# %%
 def prior_probabilities(q1_label):
     number_of_yes, number_of_no = 0, 0
     
@@ -38,47 +36,37 @@ def prior_probabilities(q1_label):
     probability_of_yes, probability_of_no = (number_of_yes/len(q1_label)), (number_of_no/len(q1_label)) 
     return probability_of_yes, probability_of_no
 
-# %%
 def conditional_probabilities(text, q1_label, SMOOTHING):
     row = len(set(q1_label))
     column = text.shape[1]
+    frequencies = np.zeros((row, column), dtype=int)
     probabilities = np.zeros((row, column), dtype=object)
     smoothing_vocabulary = SMOOTHING * column
     
-    # total number of words in each class
     total_row, total_column = text.shape
-    total_words_in_yes, total_words_in_no = 0, 0
-    for r in range(total_row):
-        # total number of words in yes
-        if (q1_label[r] == "yes"):
-            for c in range(total_column):
-                total_words_in_yes = total_words_in_yes + text[r][c][1]
-        # total number of words in no
-        else: 
-            for c in range(total_column):
-                total_words_in_no = total_words_in_no + text[r][c][1]
-    
-    # frequency of word per class
-    for c in range(column):
-        number_of_word_in_yes, number_of_word_in_no = 0, 0
-        for r in range(row):
-            # frequency of word in yes
+    total_yes, total_no = 0, 0
+    for r in range(total_row): 
+        for c in range(total_column):
+            # "yes" class
             if (q1_label[r] == "yes"):
-                number_of_word_in_yes = number_of_word_in_yes + text[r][c][1]
-            # frequency of word in no
+                total_yes = total_yes + text[r][c][1]
+                frequencies[0][c] = frequencies[0][c] + text[r][c][1]
+            # "no" class
             else:
-                number_of_word_in_no = number_of_word_in_no + text[r][c][1]
-        
-        #row 1 = yes
-        probability_with_class_yes = (number_of_word_in_yes + SMOOTHING)/(total_words_in_yes + smoothing_vocabulary)
-        probabilities[0][c] = (text[0][c][0], probability_with_class_yes)
-        #row 2 = no
-        probability_with_class_no = (number_of_word_in_no + SMOOTHING)/(total_words_in_no + smoothing_vocabulary)
-        probabilities[1][c] = (text[0][c][0], probability_with_class_no)
-    
+                total_no = total_no + text[r][c][1]
+                frequencies[1][c] = frequencies[1][c] + text[r][c][1]
+                
+    for r in range(total_row): 
+        for c in range(total_column):
+            # "yes" class
+            probability = (frequencies[0][c] + SMOOTHING)/(total_yes + smoothing_vocabulary)
+            probabilities[0][c] = (text[r][c][0], probability)
+            # "no" class
+            probability = (frequencies[1][c] + SMOOTHING)/(total_no + smoothing_vocabulary)
+            probabilities[1][c] = (text[r][c][0], probability)
+
     return probabilities
 
-# %%
 def trim_vocabulary(conditionals, text):
     matches = []
     
@@ -92,19 +80,18 @@ def trim_vocabulary(conditionals, text):
     
     return matches
 
-# %%
 def class_prediction(yes, no, conditionals, text, matches): 
     text_row = text.shape[0]
     prediction = []
     
     for tr in range(text_row):
-        score_yes, score_no = math.log(yes), math.log(no)
+        score_yes, score_no = math.log(yes, 10), math.log(no, 10)
         for i in range(len(matches)):
             text_index = matches[i][0]
             conditional_index = matches[i][1]
             if (text[tr][text_index][1] > 0):
-                score_yes = score_yes + math.log(conditionals[0][conditional_index][1])
-                score_no = score_no + math.log(conditionals[1][conditional_index][1])
+                score_yes = score_yes + math.log(conditionals[0][conditional_index][1], 10)
+                score_no = score_no + math.log(conditionals[1][conditional_index][1], 10)
         if (score_yes > score_no):
             prediction.append("yes")
         else:
@@ -112,7 +99,19 @@ def class_prediction(yes, no, conditionals, text, matches):
     
     return prediction
 
-# %%
+def metrics(y_true, y_pred):
+    precision_no = precision_score(y_true, y_pred, pos_label="no")
+    precision_yes = precision_score(y_true, y_pred, pos_label="yes")
+    recall_no = recall_score(y_true, y_pred, pos_label="no")
+    recall_yes = recall_score(y_true, y_pred, pos_label="yes")
+    f1_no = f1_score(y_true, y_pred, pos_label="no")
+    f1_yes = f1_score(y_true, y_pred, pos_label="yes")
+    
+    accuracy = accuracy_score(y_true, y_pred)
+    
+    print(classification_report(y_true, y_pred))
+    print(confusion_matrix(y_true, y_pred))
+
 def data_processing(training_file, testing_file, SMOOTHING):
     # training: hypothesis and evidence
     train_dataset = (pd.read_csv(training_file, sep='\t')).to_numpy()
@@ -132,19 +131,15 @@ def data_processing(training_file, testing_file, SMOOTHING):
     test_text = word_tokenization(test_dataset[:,1])
     test_q1_label = test_dataset[:,2]
     
-    
     # return index of matching words
     matches = trim_vocabulary(conditionals, test_text)
     
     # prediction
     prediction = class_prediction(yes, no, conditionals, test_text, matches)
     
-    print(prediction)
+    metrics(test_q1_label, prediction)
 
-# %%
 # processing datasets
 training = '../data/covid_training.tsv'
 testing = '../data/covid_test_public.tsv'
 data_processing(training, testing, 0.01)
-
-# %%
